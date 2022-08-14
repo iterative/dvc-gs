96aaf299a60da585111c6de4818db3c3b51943d3
Ruslan Kuprieiev <kupruser@gmail.com>
2022-08-14 11:40:56 +0300

    tests: enable import tests

--- dvc_gs/tests/test_dvc.py
+++ dvc_gs/tests/test_dvc.py
@@ -3,52 +3,28 @@ from dvc.testing.test_api import TestAPI  # noqa, pylint: disable=unused-import
 from dvc.testing.test_remote import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
-from dvc.testing.test_workspace import (  # noqa, pylint: disable=unused-import
-    TestAdd,
-    TestImport,
-)
+from dvc.testing.test_workspace import TestImport as _TestImport
 
 
 @pytest.fixture
-def cloud_name():
-    return "gs"
+def remote(make_remote):
+    yield make_remote(name="upstream", typ="gs")
 
 
 @pytest.fixture
-def remote(make_remote, cloud_name):
-    yield make_remote(name="upstream", typ=cloud_name)
+def workspace(make_workspace):
+    yield make_workspace(name="workspace", typ="gs")
 
 
-@pytest.fixture
-def workspace(make_workspace, cloud_name):
-    yield make_workspace(name="workspace", typ=cloud_name)
+class TestImport(_TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        return "2aa17f8daa26996b3f7a4cf8888ac9ac"
 
+    @pytest.fixture
+    def is_object_storage(self):
+        return True
 
-@pytest.fixture
-def stage_md5():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def is_object_storage():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def dir_md5():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def hash_name():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def hash_value():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    pytest.skip("not supported")
+    @pytest.fixture
+    def dir_md5(self):
+        return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
