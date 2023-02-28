12531939eff19f82e8f249584ed6f98938164dc7
Todd Jordan <86629649+tnjordan@users.noreply.github.com>
2023-02-28 13:37:29 -0800

    Empty string as default for project
    Fix for issue https://github.com/iterative/dvc-gs/issues/35.

--- dvc_gs/__init__.py
+++ dvc_gs/__init__.py
@@ -22,7 +22,7 @@ class GSFileSystem(ObjectFileSystem):
     def _prepare_credentials(self, **config):
         login_info = {"consistency": None}
         login_info["version_aware"] = config.get("version_aware", False)
-        login_info["project"] = config.get("projectname")
+        login_info["project"] = config.get("projectname", '')
         login_info["token"] = config.get("credentialpath")
         login_info["endpoint_url"] = config.get("endpointurl")
         login_info["session_kwargs"] = {"trust_env": True}
